496aa0bd6e89143b7339b70a629d0110e5c9aa58
Jeremiah Snapp <jeremiah@chef.io>
2022-05-22 03:52:15 -0400

    Disable fatal_transitive_dependency_licensing_warnings
    Signed-off-by: Jeremiah Snapp <jeremiah@chef.io>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -49,5 +49,5 @@ fetcher_read_timeout 120
 # software_gems ['omnibus-software', 'my-company-software']
 # local_software_dirs ['/path/to/local/software']
 
-fatal_transitive_dependency_licensing_warnings true
+fatal_transitive_dependency_licensing_warnings false
 fips_mode (ENV["OMNIBUS_FIPS_MODE"] || "").casecmp("true") >= 0
